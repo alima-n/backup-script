81439ef553d90ae299dbf1c45ef39dc1ad1079ec
Alima Nurgazieva <anurgazieva@gmail.com>
2022-01-30 14:45:09 +0100

    added openSSL encryption

--- backup.sh
+++ backup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 DIRECTORY=$1
-FILENAME=$3
 [[ $2 != 'none' ]] && COMPRESSION=--$2 || COMPRESSION=''
+FILENAME=$3
 
 echo "Starting backup of $DIRECTORY $(date)"
 
-tar -c $COMPRESSION --file=$FILENAME -P $DIRECTORY > /dev/null 2> error.log
+{
+  tar -c $COMPRESSION -P $DIRECTORY | openssl enc -e -aes256 -out $FILENAME
+} > /dev/null 2> error.log
 
 echo "Backup completed $(date)"
